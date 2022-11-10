701824ff786e57276f93f8528ab319f8a24a2db3
Lucas3363 <90220756+Lucas3363@users.noreply.github.com>
2022-11-10 20:35:55 -0300

    teste

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return <div className="App">
+      <h1>
+        Salve
+      </h1>
+      <ul>
+        <li>um</li>
+        <li>dois</li>
+        <li>tres</li>
+        <li>quatro</li>
+      </ul>
+    </div>
+}
+
+export default App;
